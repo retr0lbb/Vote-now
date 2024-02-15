e4913612a91a3dd95f2f017a6abb9a10ae285352
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-02-14 21:01:04 -0300

    add analitics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Analytics } from '@vercel/analytics/react';
 import './index.css'
 import React from 'react'
 import { Toaster } from "@/components/ui/sonner.tsx"
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
         <Toaster />
+        <Analytics/>
     </React.StrictMode>
 )
